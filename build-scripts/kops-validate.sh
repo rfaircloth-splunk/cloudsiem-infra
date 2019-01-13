0f714b39554a18ed165ac57e3263bd9ab02951f3
Ryan Faircloth <rfaircloth@splunk.com>
2019-01-13 12:39:12 -0800

    build update

--- build-scripts/kops-validate.sh
+++ build-scripts/kops-validate.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-until host api.${KOPS_CLUSTER_NAME} > /dev/null
-do
-    echo "No ip for api.${KOPS_CLUSTER_NAME}"
-    sleep 10
-done
+#until host api.${KOPS_CLUSTER_NAME} > /dev/null
+#do
+#    echo "No ip for api.${KOPS_CLUSTER_NAME}"
+#    sleep 10
+#done
 
 until kops validate cluster
 do
