d9830814783d83bc87373aeb492870ab380b580f
Ryan Faircloth <rfaircloth@splunk.com>
2019-01-13 11:28:05 -0800

    build update

--- build-scripts/install-dependencies.sh
+++ build-scripts/install-dependencies.sh
@@ -12,6 +12,7 @@ easy_install pip
 echo -e "${GREEN}==== INSTALLING KUBECTL ====${NC}"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
 echo -e "${GREEN}==== SUCCESSFULLY INSTALLED KUBECTL ====${NC}"
 echo ''
 
@@ -19,6 +20,7 @@ echo ''
 echo -e "${GREEN}==== INSTALLING KOPS ====${NC}"
 curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x ./kops-linux-amd64
+mv ./kops-linux-amd64 /usr/local/bin/kops
 echo -e "${GREEN}==== SUCCESSFULLY INSTALLED KOPS ====${NC}"
 echo ''
 
@@ -32,9 +34,9 @@ echo ''
 
 # install terraform
 echo -e "${GREEN}==== INSTALLING TERRAFORM ====${NC}"
-apt-get install unzip
 curl -LO https://releases.hashicorp.com/terraform/0.11.2/terraform_0.11.2_linux_amd64.zip
 unzip terraform_0.11.2_linux_amd64.zip -d ./
 chmod +x ./terraform
+mv terraform /us/local/bin/terraform
 echo -e "${GREEN}==== SUCCESSFULLY INSTALLED TERRAFORM ====${NC}"
 echo ''
